6f46b197ff5806a90374b3e07f1af86051f56e67
jyi <joshua.yi@amadeus.com>
2018-10-04 15:30:02 -0400

    Add types for weather

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -2,3 +2,42 @@ export interface StoreState {
     languageName: string; // programming language app was written in
     enthusiasmLevel: number; // varies
 }
+
+export interface WeatherState {
+    city: string;
+    scale: TempScale
+    currentForecast: Forecast;
+    // Forecasts are in 3 hour increments
+    forecasts: Forecast[];
+}
+
+// Each forecast is 3 hours of weather
+export interface Forecast {
+    time: number;
+    weather: Weather
+    temperature: number;
+    humidity: number;
+    precipitation: number;
+    wind: Wind;
+}
+
+export interface Wind {
+    speed: number;
+    deg: number;
+}
+
+export enum TempScale {
+    C,
+    F
+}
+
+export enum Weather {
+    ClearSky,
+    // Clouds
+    BrokenClouds,
+    ScatteredClouds,
+    FewClouds,
+    // Rain
+    LightRain,
+    ModerateRain
+}

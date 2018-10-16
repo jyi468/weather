3c956827a405d88d66615866a38cde04234f5263
jyi <joshua.yi@amadeus.com>
2018-10-16 17:23:57 -0400

    Add chart

--- src/components/weatherChart/WeatherChart.tsx
+++ src/components/weatherChart/WeatherChart.tsx
@@ -20,97 +20,156 @@ export class WeatherChart extends React.Component<ChartProps, WeatherState> {
         super(props);
     }
 
-    // Do not update component if same day
-    /*shouldComponentUpdate(nextProps: ChartProps) {
-        if (nextProps) {
-            return (nextProps.dayIndex !== this.props.dayIndex) ||
-                (nextProps.chartType !== this.props.chartType);
-        }
-
-        return true;
-    }*/
-
     render() {
-        let { days, dayIndex, scale, changeHour, chartType} = this.props;
-        let points: any = [];
-        if (days) {
-            points = days[dayIndex].hours.map((forecast: any) => {
-                return WeatherUtils.getTemperature(forecast.temperature, scale);
-            });
-        }
+        let {days, dayIndex, scale, changeHour, chartType} = this.props;
 
-        let ctx = document.getElementById("myChart") as HTMLCanvasElement;
-        // Fix: do not re render when point is clicked
-        if (ctx && chartType === ChartType.Temperature) {
-            //let progress = document.getElementById('animationProgress');
-            const myChart: Chart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ['2 AM', '5 AM', '8 AM', '11 AM', '2 PM', '5 PM', '8 PM', '11 PM'],
-                    datasets: [{
-                        label: 'Temperature',
-                        data: points,
-                        borderColor: '#FFCB34',
-                        borderWidth: 2,
-                        backgroundColor: '#FFF5D6'
-                    }]
-                },
-                options: {
-                    plugins: {
-                        datalabels: {
-                            backgroundColor: function (context: any) {
-                                return context.dataset.backgroundColor;
+        if (days) {
+            switch (chartType) {
+                case ChartType.Temperature:
+                    let tempCtx = document.getElementById("tempChart") as HTMLCanvasElement || {};
+                    let tPoints: any[] = days[dayIndex].hours.map((forecast: any) => {
+                        return WeatherUtils.getTemperature(forecast.temperature, scale);
+                    });
+                    const tChart = new Chart(tempCtx, {
+                        type: 'line',
+                        data: {
+                            labels: ['2 AM', '5 AM', '8 AM', '11 AM', '2 PM', '5 PM', '8 PM', '11 PM'],
+                            datasets: [{
+                                label: 'Temperature',
+                                data: tPoints,
+                                borderColor: '#FFCB34',
+                                borderWidth: 2,
+                                backgroundColor: '#FFF5D6'
+                            }]
+                        },
+                        options: {
+                            plugins: {
+                                datalabels: {
+                                    backgroundColor: function (context: any) {
+                                        return context.dataset.backgroundColor;
+                                    },
+                                    borderRadius: 4,
+                                    color: 'white',
+                                    font: {
+                                        weight: 'bold'
+                                    },
+                                    formatter: Math.round
+                                }
                             },
-                            borderRadius: 4,
-                            color: 'white',
-                            font: {
-                                weight: 'bold'
+                            onClick: (e: MouseEvent) => {
+                                const point: ChartElement = tChart.getElementAtEvent(e)[0];
+                                if (point) {
+                                    const hourIndex = point._index;
+                                    changeHour(hourIndex);
+                                }
                             },
-                            formatter: Math.round
-                        }
-                    },
-                    onClick: (e: MouseEvent) => {
-                        const point: ChartElement = myChart.getElementAtEvent(e)[0];
-                        if (point) {
-                            const hourIndex = point._index;
-                            //if (hourIndex !== currentHour) {
-                            changeHour(hourIndex);
-                            //}
-                        }
-                    },
-                    legend: {
-                        display: false
-                    },
-                    scales: {
-                        xAxes: [{
-                            gridLines: {
-                                drawBorder: false,
+                            legend: {
                                 display: false
+                            },
+                            scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        drawBorder: false,
+                                        display: false
+                                    }
+                                }],
+                                yAxes: [{
+                                    gridLines: {
+                                        drawBorder: false,
+                                        display: false
+                                    },
+                                    ticks: {
+                                        display: false
+                                    }
+                                }]
+                            },
+                            tooltips: {
+                                callbacks: {
+                                    label: (item) => `${item.yLabel} ${scale}`
+                                }
                             }
-                        }],
-                        yAxes: [{
-                            gridLines: {
-                                drawBorder: false,
+                        }
+                    });
+                    break;
+                case ChartType.Precipitation:
+                    let precipCtx = document.getElementById("precipChart") as HTMLCanvasElement || {};
+                    let pPoints: any[] = days[dayIndex].hours.map((forecast: any) => {
+                        return (forecast.precipitation * 0.0393701).toFixed(2);
+                    });
+                    const pChart: Chart = new Chart(precipCtx, {
+                        type: 'bar',
+                        data: {
+                            labels: ['2 AM', '5 AM', '8 AM', '11 AM', '2 PM', '5 PM', '8 PM', '11 PM'],
+                            datasets: [{
+                                label: 'Precipitation',
+                                data: pPoints,
+                                borderColor: '#FFCB34',
+                                borderWidth: 2,
+                                backgroundColor: '#FFF5D6'
+                            }]
+                        },
+                        options: {
+                            plugins: {
+                                datalabels: {
+                                    backgroundColor: function (context: any) {
+                                        return context.dataset.backgroundColor;
+                                    },
+                                    borderRadius: 4,
+                                    color: 'white',
+                                    font: {
+                                        weight: 'bold'
+                                    },
+                                    formatter: Math.round
+                                }
+                            },
+                            onClick: (e: MouseEvent) => {
+                                const point: ChartElement = pChart.getElementAtEvent(e)[0];
+                                if (point) {
+                                    const hourIndex = point._index;
+                                    changeHour(hourIndex);
+                                }
+                            },
+                            legend: {
                                 display: false
                             },
-                            ticks: {
-                                display: false
+                            scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        drawBorder: false,
+                                        display: false
+                                    }
+                                }],
+                                yAxes: [{
+                                    gridLines: {
+                                        drawBorder: false,
+                                        display: false
+                                    },
+                                    ticks: {
+                                        display: false
+                                    }
+                                }]
+                            },
+                            tooltips: {
+                                callbacks: {
+                                    label: (item) => `${item.yLabel} in.`
+                                }
                             }
-                        }]
-                    },
-                    tooltips: {
-                        callbacks: {
-                            label: (item) => `${item.yLabel} ${scale}`
                         }
-                    }
-                }
-            });
+                    });
+                    break;
+                case ChartType.Wind:
+
+            }
         }
 
         return (
             <div className="card p-3">
                 <div className="card-img-top">
-                    {<canvas id="myChart" width="400" height="75"></canvas>}
+                    {(chartType === undefined ||
+                        chartType === ChartType.Temperature) &&
+                    <canvas id="tempChart" width="400" height="75"></canvas>}
+                    {(chartType === undefined || chartType === ChartType.Precipitation) &&
+                    <canvas id="precipChart" width="400" height="75"></canvas>}
                 </div>
             </div>
 
